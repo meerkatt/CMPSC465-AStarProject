b2dcc0dc2144952ec8a51106bcb4a262212fbae4
Nabeel Ahmed <nabeelahmed1721@gmail.com>
2024-11-11 19:44:25 -0500

    basic gui

---
+++ GUI.java
@@ -0,0 +1,26 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GUI {
+    private final int WIDTH;
+    private final int HEIGHT;
+    private final JFrame frame;
+
+
+    public GUI(int width, int height, String title) {
+        WIDTH = width;
+        HEIGHT = height;
+
+        frame = new JFrame(title); 
+        frame.setSize(WIDTH, HEIGHT);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void add(JPanel panel) {
+        frame.add(panel);
+    }
+
+    public void show() {
+        frame.setVisible(true);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+import javax.swing.JPanel;
+
+public class Main{
+    public static void main(String[] args) {
+        GUI gui = new GUI(800, 600, "AStar Project");
+        gui.add(new JPanel());
+        gui.show();
+    }
+}
