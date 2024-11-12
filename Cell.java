ae7b52251e6ca38cbc9c69be2c441a22bcab179b
Nabeel Ahmed <nabeelahmed1721@gmail.com>
2024-11-12 15:31:18 -0500

    can generate a simple maze

--- Cell.java
+++ Cell.java
@@ -4,6 +4,7 @@ import java.awt.Graphics;
  class Cell {
     public int x;
     public int y;
+    public boolean isWall = false;
     public static final int WIDTH = 25;
     public static final int HEIGHT = 25;
 
@@ -11,8 +12,19 @@ import java.awt.Graphics;
         this.x = x;
         this.y = y;
     }
+    
     public void render(Graphics g) {
         g.setColor(Color.BLACK);
-        g.drawRect(x * WIDTH, y * HEIGHT, WIDTH, HEIGHT);
+
+        if (this.isWall) {
+            g.fillRect(x * WIDTH, y * HEIGHT, WIDTH, HEIGHT);
+        } else {
+            g.drawRect(x * WIDTH, y * HEIGHT, WIDTH, HEIGHT);
+        }
+        
     }
+    
+    public void setWall(boolean isWall) {
+        this.isWall = isWall;
+    }   
 }
