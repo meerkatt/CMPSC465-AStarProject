0bfd69c3b313fdf27e4d4a40e4e13f5ec88bca02
meerkatt <meergujral@gmail.com>
2024-11-11 22:38:57 -0500

    Node Class Lightly Tested

--- TestCases.java
+++ TestCases.java
@@ -21,6 +21,29 @@ public class TestCases {
                 return "Not all neighbors were set";
             }
         }
+
+        return "Neighbors were sucessfully set!";
+    }
+
+    public String settingNodeNeighborsConstructor()
+    {
+        Node test = new Node();
+        Node n = new Node();
+        Node e = new Node();
+        Node s = new Node();
+        Node w = new Node();
+        test.setNeighbors(n,e,s,w);
+        Node[] neighborCheck = test.getNeighbors();
+        Node testNextNode = new Node(neighborCheck,test);
+        Node[] secondNeighborCheck = testNextNode.getNeighbors();
+        for(Node element:secondNeighborCheck)
+        {
+            if(element == null)
+            {
+                return "Not all neighbors were set";
+            }
+        }
+
         return "Neighbors were sucessfully set!";
     }
 }
