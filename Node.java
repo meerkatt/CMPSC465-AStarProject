67d33411b4e7b7453d63feb03e8c451dec3f3be5
Nabeel Ahmed <nabeelahmed1721@gmail.com>
2024-11-14 16:15:47 -0500

    cleaned up code

--- Node.java
+++ Node.java
@@ -1,6 +1,6 @@
 
 
-public class Node implements Comparable{
+public class Node implements Comparable {
     private Node[] neighborNodes; // Order of Nodes: North, East, South, West
 
     private double gCost;
@@ -13,164 +13,118 @@ public class Node implements Comparable{
 
     ////Constructors
 
-    public Node()
-    {
+    public Node() {
         this.neighborNodes = new Node[4];
-        for(Node element:this.neighborNodes)
-        {
-            element = null;
-        }
         this.previousNode = null;
-
     }
 
-    public Node(int x, int y)
-    {
+    public Node(int x, int y) {
         this.xCoord = x;
         this.yCoord = y;
     }
 
-    public Node(Node[] neighbors, Node previous)
-    {
+    public Node(Node[] neighbors, Node previous) {
         this.neighborNodes = neighbors;
         this.previousNode = previous;
     }
 
     //// Comparator
-    public int compareTo(Object otherObject)
-    {
+    @Override
+    public int compareTo(Object otherObject) {
         Node otherNode = (Node) otherObject;
-        if(this.totalCost < otherNode.getTotalCost())
-        {
-            return -1;
+
+        int totalCostComparison =  Double.compare(this.totalCost, otherNode.getTotalCost());
+        if (totalCostComparison != 0) {
+            return totalCostComparison;
         }
-        else if (this.totalCost > otherNode.getTotalCost()) 
-        {
-            return 1;
+
+        int gCostComparison = Double.compare(this.gCost, otherNode.getGCost());
+        if (gCostComparison != 0) {
+            return gCostComparison;
         }
-        else{
-            if(this.gCost < otherNode.getGCost())
-            {
-                return -1;
-            }
-            else if (this.gCost > otherNode.getGCost()) 
-            {
-                return 1;
-            }
-            else{
-                
-                if(this.hCost > otherNode.getHCost()) 
-                {
-                    return 1;
-                }
-                else if(this.hCost < otherNode.getHCost())
-                {
-                    return -1;
-                }else
-                {
-                    return 0;
-                }
-            }
-        }
-        
+
+        return Double.compare(this.hCost, otherNode.getHCost());
     }
 
-
     //// Mutators
-
-    public void setNeighbors(Node[] neighbors)
-    {
+    public void setNeighbors(Node[] neighbors) {
         this.neighborNodes = neighbors;
     }
 
-    public void setNeighbors(Node north, Node east, Node south, Node west)
-    {
+    public void setNeighbors(Node north, Node east, Node south, Node west) {
         this.neighborNodes = new Node[]{north, east, south, west};
     }
 
-    public void deleteNeighbors()
-    {
+    public void deleteNeighbors() {
         this.neighborNodes = new Node[4];
     }
 
-    public void setPreviousNode(Node previous)
-    {
+    public void setPreviousNode(Node previous) {
         this.previousNode = previous;
     }
 
-    public void setGCost(double gCostValue)
-    {
+    public void setGCost(double gCostValue) {
         this.gCost = gCostValue;
     }
 
-    public void setHCost(double hCostValue)
-    {
+    public void setHCost(double hCostValue) {
         this.hCost = hCostValue;
     }
 
-    public void setTotalCost(double totalCostValue)
-    {
+    public void setTotalCost(double totalCostValue) {
         this.totalCost = totalCostValue;
     }
 
-    public void setXY(int x, int y)
-    {
+    public void setXY(int x, int y) {
         this.xCoord = x;
         this.yCoord = y;
     }
 
-    public void resetCost(){
+    public void resetCost() {
         this.gCost = 0.0;
         this.hCost = 0.0;
         this.totalCost = 0.0;
     }
 
     //// Accessors
-    public Node[] getNeighbors()
-    {
+    public Node[] getNeighbors() {
         return neighborNodes;
     }
 
-    public double getGCost()
-    {
+    public double getGCost() {
         return gCost;
     }
 
-    public double getHCost()
-    {
+    public double getHCost() {
         return hCost;
     }
 
-    public double getTotalCost()
-    {
+    public double getTotalCost() {
         return totalCost;
     }
 
-    public int getXCoord()
-    {
+    public int getXCoord() {
         return xCoord;
     }
 
-    public int getYCoord()
-    {
+    public int getYCoord() {
         return yCoord;
     }
 
-    public Node getPreviousNode()
-    {
+    public Node getPreviousNode() {
         return previousNode;
     }
 
     ////Methods
-    public boolean isNeighbor(Node checkNode)
-    {
-        for(Node element:neighborNodes){
-            if(element.getXCoord() == checkNode.getXCoord() && element.getYCoord() == checkNode.getYCoord())
-            {
+    public boolean isNeighbor(Node checkNode) {
+        for(Node element:neighborNodes) {
+            if (
+                element.getXCoord() == checkNode.getXCoord() &&
+                element.getYCoord() == checkNode.getYCoord()
+            ) {
                 return true;
             }
         }
         return false;
     }
-
 }
